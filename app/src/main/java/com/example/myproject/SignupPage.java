4ea9a166e8e739d5064af3755ee85edb4bd28f21
asrjashan <jashanchhapa85@gmail.com>
2024-07-07 10:03:27 -0400

    create user class

--- app/src/main/java/com/example/myproject/SignupPage.java
+++ app/src/main/java/com/example/myproject/SignupPage.java
@@ -87,5 +87,7 @@ public class SignupPage extends AppCompatActivity {
         }
 
 
+
+
     }
 }
---
+++ app/src/main/java/com/example/myproject/User.java
@@ -0,0 +1,15 @@
+package com.example.myproject;
+
+public class User {
+    public String name;
+    public String email;
+    public String username;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
