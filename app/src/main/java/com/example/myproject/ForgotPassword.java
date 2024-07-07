eb7c42f0fcca79b5c5dc14a302bf7c1a1250eccf
asrjashan <jashanchhapa85@gmail.com>
2024-07-07 10:57:46 -0400

    forgot password page done

---
+++ app/src/main/java/com/example/myproject/ForgotPassword.java
@@ -0,0 +1,43 @@
+package com.example.myproject;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    android.widget.EditText editText;
+    android.widget.Button resetbutton;
+    com.google.firebase.auth.FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        editText = findViewById(com.example.myproject.R.id.emailInput);
+        resetbutton = findViewById(com.example.myproject.R.id.resetPasswordButton);
+        mAuth = com.google.firebase.auth.FirebaseAuth.getInstance();
+
+        resetbutton.setOnClickListener(v -> resetPassword());
+    }
+
+    private void resetPassword() {
+        String email = editText.getText().toString().trim();
+
+        if (android.text.TextUtils.isEmpty(email) || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editText.setError("Valid email is required");
+            return;
+        }
+
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                android.widget.Toast.makeText(ForgotPassword.this, "Password reset email sent", android.widget.Toast.LENGTH_SHORT).show();
+
+            } else {
+                android.widget.Toast.makeText(ForgotPassword.this, "Error sending password reset email", android.widget.Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+}
