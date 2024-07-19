f8ca70aa5ab573e523497895f64a2a2af6df45f0
asrjashan <jashanchhapa85@gmail.com>
2024-07-19 10:52:18 -0400

    create hotel viewholder class

--- app/src/main/java/com/example/myproject/HotelAdapter.java
+++ app/src/main/java/com/example/myproject/HotelAdapter.java
@@ -13,4 +13,24 @@ public class HotelAdapter extends androidx.recyclerview.widget.RecyclerView.Adap
     interface OnHotelClickListener {
         void onHotelClick(Hotel hotel);
     }
-}
+
+    public HotelViewHolder onCreateViewHolder(@androidx.annotation.NonNull android.view.ViewGroup parent, int viewType) {
+        android.view.View view = android.view.LayoutInflater.from(parent.getContext()).inflate(com.example.myproject.R.layout.activity_hotel_data, parent, false);
+        return new HotelViewHolder(view);
+    }
+
+    class HotelViewHolder extends androidx.recyclerview.widget.RecyclerView.ViewHolder {
+        private android.widget.TextView nameTextView, locationTextView, ratingTextView;
+        private android.widget.ImageView hotelImageView;
+
+        HotelViewHolder(@androidx.annotation.NonNull android.view.View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.nameTextView);
+            locationTextView = itemView.findViewById(R.id.locationTextView);
+            ratingTextView = itemView.findViewById(R.id.ratingTextView);
+            hotelImageView = itemView.findViewById(R.id.hotelImageView);
+            itemView.setOnClickListener(v -> onHotelClickListener.onHotelClick(hotelList.get(getAdapterPosition())));
+        }
+
+    }
+
