4d3b0a8ab5f362d2cdef578e9b9c16fb89f1e13f
asrjashan <jashanchhapa85@gmail.com>
2024-07-20 10:03:06 -0400

    main activity profile button

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button_profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_profile = findViewById(R.id.btn_profile);
+
+        button_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ProfilePage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 }
