e0957a9461195333f77381cbfdc78fcfd3a38a98
asrjashan <jashanchhapa85@gmail.com>
2024-07-20 12:53:04 -0400

    SignOutPage done

--- app/src/main/java/com/example/myproject/SignOutPage.java
+++ app/src/main/java/com/example/myproject/SignOutPage.java
@@ -1,30 +1,30 @@
 package com.example.myproject;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-public class SignOutActivity extends AppCompatActivity {
-
-    private Button signOutButton;
-
+public class SignOutPage extends AppCompatActivity {
+     android.widget.Button signOutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.example.myproject.R.layout.activity_sign_out_page);
 
+        setContentView(R.layout.activity_sign_out_page);
         signOutButton = findViewById(R.id.sign_out_button);
-       signOutButton.setOnClickListener(new android.view.View.OnClickListener() {
+        signOutButton.setOnClickListener(new android.view.View.OnClickListener() {
            @Override
            public void onClick(android.view.View v) {
-               FirebaseAuth.getInstance().signOut();
-               startActivity(new Intent(SignOutActivity.this, LoginPage.class));
+               com.google.firebase.auth.FirebaseAuth.getInstance().signOut();
+               android.content.Intent intent = new android.content.Intent(SignOutPage.this,LoginPage.class);
+               startActivity(intent);
                finish();
            }
        });
+
     }
 }
