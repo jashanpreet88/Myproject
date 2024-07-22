d41fbc093be2545615543c50f6510077c99aa1ca
asrjashan <jashanchhapa85@gmail.com>
2024-07-22 18:19:10 -0400

    oncreateOptionMenu and onOptionItemsselected method

--- app/src/main/java/com/example/myproject/AdminActivity.java
+++ app/src/main/java/com/example/myproject/AdminActivity.java
@@ -15,10 +15,30 @@ public class AdminActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        setTitle(" Admin Room-Rover");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        android.view.MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@androidx.annotation.NonNull android.view.MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.menu_profile) {
+            startActivity(new android.content.Intent(AdminActivity.this, ProfilePage.class));
+            return true;
+        } else if (itemId == R.id.menu_sign_out) {
+            startActivity(new android.content.Intent(AdminActivity.this, SignOutPage.class));
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
