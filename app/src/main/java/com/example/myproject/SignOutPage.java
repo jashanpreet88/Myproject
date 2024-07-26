71c715e345590b3590a287b44cbe2d7de52655ed
asrjashan <jashanchhapa85@gmail.com>
2024-07-25 22:05:49 -0400

    SignOutPage java code

--- app/src/main/java/com/example/myproject/SignOutPage.java
+++ app/src/main/java/com/example/myproject/SignOutPage.java
@@ -1,6 +1,9 @@
 package com.example.myproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,23 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignOutPage extends AppCompatActivity {
-     android.widget.Button signOutButton;
+    Button button_sign_out, button_go_back;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_sign_out_page);
-        signOutButton = findViewById(R.id.sign_out_button);
-        signOutButton.setOnClickListener(new android.view.View.OnClickListener() {
-           @Override
-           public void onClick(android.view.View v) {
-               com.google.firebase.auth.FirebaseAuth.getInstance().signOut();
-               android.content.Intent intent = new android.content.Intent(SignOutPage.this,LoginPage.class);
-               startActivity(intent);
-               finish();
-           }
-       });
+        button_sign_out = findViewById(R.id.sign_out);
+        button_go_back = findViewById(R.id.go_back);
+
+        button_go_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
     }
 }
