ebc96b6fc8c9e5281939ec2acf61c711e390eebe
asrjashan <jashanchhapa85@gmail.com>
2024-07-31 19:50:13 -0400

    create getview  methods in AdminHotelAdapter

--- app/src/main/java/com/example/myproject/AdminHotelAdapter.java
+++ app/src/main/java/com/example/myproject/AdminHotelAdapter.java
@@ -1,23 +1,45 @@
 package com.example.myproject;
 
 public class AdminHotelAdapter extends android.widget.BaseAdapter {
+
+    android.content.Context context;
+    java.util.List<Hotel>hotels;
+    com.google.firebase.database.DatabaseReference HotelDB;
+
+    public  AdminHotelAdapter(android.content.Context context, java.util.List<Hotel> hotels){
+        this.context =context;
+        this.hotels = hotels;
+        HotelDB = com.google.firebase.database.FirebaseDatabase.getInstance().getReference("hotels");
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return hotels.size();
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public Object getItem(int i) {
+        return hotels.get(i);
     }
 
     @Override
-    public long getItemId(int position) {
-        return 0;
+    public long getItemId(int i) {
+        return i;
     }
 
     @Override
-    public android.view.View getView(int position, android.view.View convertView, android.view.ViewGroup parent) {
-        return null;
+    public android.view.View getView(int i, android.view.View view, android.view.ViewGroup viewGroup) {
+        if (view == null) {
+            view = android.view.LayoutInflater.from(context).inflate(R.layout.admin_hotel_item, viewGroup, false);
+        }
+        final Hotel hotel = hotels.get(i);
+        android.widget.ImageView imageView = view.findViewById(R.id.hotel_image);
+        android.widget.TextView textView = view.findViewById(R.id.hotel_name);
+        com.squareup.picasso.Picasso.get()
+                .load(hotel.getImageUrl())
+                .into(imageView);
+        textView.setText(hotel.getName());
+
+        return view;
     }
 }
