d9091e3dbe5ac4234507534cf748269b7d80d414
asrjashan <jashanchhapa85@gmail.com>
2024-07-31 23:47:31 -0400

    create constructor in HotelAdapter class

--- app/src/main/java/com/example/myproject/HotelAdapter.java
+++ app/src/main/java/com/example/myproject/HotelAdapter.java
@@ -1,6 +1,16 @@
 package com.example.myproject;
 
 public class HotelAdapter extends android.widget.BaseAdapter {
+
+    private android.content.Context context;
+    private java.util.List<Hotel>hotels;
+    private com.google.firebase.database.DatabaseReference HotelsDB;
+
+    public  HotelAdapter(android.content.Context context, java.util.List<Hotel>hotels){
+        this.context=context;
+        this.hotels=hotels;
+        HotelsDB = com.google.firebase.database.FirebaseDatabase.getInstance().getReference("hotels");
+    }
     @Override
     public int getCount() {
         return 0;
