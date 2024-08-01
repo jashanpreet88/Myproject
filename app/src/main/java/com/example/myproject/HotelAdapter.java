985ed0f5aa1cf30e8f7ae407209df6d13fe6e054
asrjashan <jashanchhapa85@gmail.com>
2024-07-31 23:42:03 -0400

    implement methods in HotelAdapter class

--- app/src/main/java/com/example/myproject/HotelAdapter.java
+++ app/src/main/java/com/example/myproject/HotelAdapter.java
@@ -1,5 +1,23 @@
 package com.example.myproject;
 
-public class HotelAdapter{
+public class HotelAdapter extends android.widget.BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
 
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public android.view.View getView(int position, android.view.View convertView, android.view.ViewGroup parent) {
+        return null;
+    }
 }
