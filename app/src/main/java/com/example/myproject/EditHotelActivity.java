d4f2f294a3f22043d26dad0dd9cb6c7a6c80d62e
asrjashan <jashanchhapa85@gmail.com>
2024-08-07 18:47:23 -0400

    create variable in edit hotel

--- app/src/main/java/com/example/myproject/EditHotelActivity.java
+++ app/src/main/java/com/example/myproject/EditHotelActivity.java
@@ -10,15 +10,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class EditHotelActivity extends AppCompatActivity {
 
+    String hotelId;
+    com.google.firebase.database.DatabaseReference HotelsDB;
+    android.widget.EditText hotel_name, hotel_location, hotel_rating, hotel_pricePerNight, hotel_available, hotel_image_url;
+    android.widget.CheckBox hotelAvailableCheckbox;
+    android.widget.Button button_update;
+    private com.google.firebase.database.DatabaseReference hotelsReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_edit_hotel);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        hotelsReference = com.google.firebase.database.FirebaseDatabase.getInstance().getReference().child("hotels");
+        hotel_name = findViewById(R.id.hotel_name_edittext);
+        hotel_location = findViewById(R.id.hotel_location_edittext);
+        hotel_rating = findViewById(R.id.hotel_rating_edittext);
+        hotel_pricePerNight = findViewById(R.id.hotel_price_edittext);
+        hotelAvailableCheckbox = findViewById(R.id.hotel_available_checkbox);
+        hotel_image_url = findViewById(R.id.hotel_image_url_edittext);
+        button_update = findViewById(R.id.update_hotel_btn);
+
     }
 }
