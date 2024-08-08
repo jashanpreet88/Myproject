3de3461b1330f882f8af910e82d28b408f8a69a9
asrjashan <jashanchhapa85@gmail.com>
2024-08-07 22:30:03 -0400

    back button in DeleteHotelActivity

--- app/src/main/java/com/example/myproject/DeleteHotelActivity.java
+++ app/src/main/java/com/example/myproject/DeleteHotelActivity.java
@@ -10,15 +10,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class DeleteHotelActivity extends AppCompatActivity {
 
+    private com.google.firebase.database.DatabaseReference HotelsDB;
+    String hotelId;
+    android.widget.Button button_delete, button_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_delete_hotel);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        button_delete = findViewById(R.id.btn_yes);
+        button_back = findViewById(R.id.btn_no);
+        hotelId = getIntent().getStringExtra("hotel_id");
+        HotelsDB = com.google.firebase.database.FirebaseDatabase.getInstance().getReference("hotels");
+
+        button_back.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                finish();
+            }
         });
     }
 }
