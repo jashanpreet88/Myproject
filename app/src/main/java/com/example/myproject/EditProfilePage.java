b0a0b91da650020479acc65c452ebbfc6944cba1
asrjashan <jashanchhapa85@gmail.com>
2024-08-14 00:01:36 -0400

    implement addListennerforsingleValueevent

--- app/src/main/java/com/example/myproject/EditProfilePage.java
+++ app/src/main/java/com/example/myproject/EditProfilePage.java
@@ -10,15 +10,36 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class EditProfilePage extends AppCompatActivity {
 
+    private com.google.firebase.database.DatabaseReference UsersDB;
+    private com.google.firebase.auth.FirebaseUser currentUser;
+    private com.google.firebase.auth.FirebaseAuth mAuth;
+    private String userId;
+    private android.widget.EditText etUsername;
+    private android.widget.Button btnEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_edit_profile_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        mAuth = com.google.firebase.auth.FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        UsersDB = com.google.firebase.database.FirebaseDatabase.getInstance().getReference("users");
+        etUsername = findViewById(R.id.tvUsername);
+        btnEdit = findViewById(R.id.btnEdit);
+
+        if (currentUser != null) {
+            userId = currentUser.getUid();
+            UsersDB.child(userId).child("name").addListenerForSingleValueEvent(new com.google.firebase.database.ValueEventListener() {
+                @Override
+                public void onDataChange(@androidx.annotation.NonNull com.google.firebase.database.DataSnapshot snapshot) {
+
+                }
+
+                @Override
+                public void onCancelled(@androidx.annotation.NonNull com.google.firebase.database.DatabaseError error) {
+
+                }
+            });
+
+        }
     }
 }
