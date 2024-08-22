73745528e3512f159918f0c63d30bb32e8c12d71
asrjashan <jashanchhapa85@gmail.com>
2024-08-22 09:52:03 -0400

    create variable and empty construstor

--- app/src/main/java/com/example/myproject/HotelBooking.java
+++ app/src/main/java/com/example/myproject/HotelBooking.java
@@ -1,4 +1,16 @@
 package com.example.myproject;
 
 public class HotelBooking {
+    private String id;
+    private String name;
+    private String location;
+    private double rating;
+    private String imageUrl;
+    private double pricePerNight;
+    private boolean available;
+    private String checkIn;
+    private String checkOut;
+
+    public HotelBooking() {
+    }
 }
