6dc046cf26314cbe70f903dd763df415ad20c684
asrjashan <jashanchhapa85@gmail.com>
2024-08-22 09:43:10 -0400

    implement constructor

--- app/src/main/java/com/example/myproject/BookingAdapter.java
+++ app/src/main/java/com/example/myproject/BookingAdapter.java
@@ -1,6 +1,13 @@
 package com.example.myproject;
 
 public class BookingAdapter  extends android.widget.BaseAdapter {
+    private android.content.Context context;
+    private java.util.List<HotelBooking> bookings;
+
+    public BookingAdapter(android.content.Context context, java.util.List<HotelBooking> bookings) {
+        this.context = context;
+        this.bookings = bookings;
+    }
     @Override
     public int getCount() {
         return 0;
