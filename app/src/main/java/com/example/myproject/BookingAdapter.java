ec1e6f855a240e74d3921aa1265b128b0a7d1a0b
asrjashan <jashanchhapa85@gmail.com>
2024-08-22 09:47:33 -0400

    BookingAdapter update

--- app/src/main/java/com/example/myproject/BookingAdapter.java
+++ app/src/main/java/com/example/myproject/BookingAdapter.java
@@ -10,21 +10,44 @@ public class BookingAdapter  extends android.widget.BaseAdapter {
     }
     @Override
     public int getCount() {
-        return 0;
+        return bookings.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return bookings.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public android.view.View getView(int position, android.view.View convertView, android.view.ViewGroup parent) {
-        return null;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_booking, parent, false);
+        }
+
+        HotelBooking booking = bookings.get(position);
+
+        android.widget.TextView name = convertView.findViewById(R.id.booking_name);
+        android.widget.TextView location = convertView.findViewById(R.id.booking_location);
+        android.widget.TextView checkInDate = convertView.findViewById(R.id.booking_check_in_date);
+        android.widget.TextView checkOutDate = convertView.findViewById(R.id.booking_check_out_date);
+        android.widget.TextView pricePerNight = convertView.findViewById(R.id.booking_price_per_night);
+        android.widget.ImageView image = convertView.findViewById(R.id.booking_image);
+
+        name.setText(booking.getName());
+        location.setText(booking.getLocation());
+        checkInDate.setText("Check-in: " + booking.getCheckIn());
+        checkOutDate.setText("Check-out: " + booking.getCheckOut());
+        pricePerNight.setText("Price per night: " + booking.getPricePerNight());
+
+        com.squareup.picasso.Picasso.get()
+                .load(booking.getImageUrl())
+                .into(image);
+
+        return convertView;
     }
 }
