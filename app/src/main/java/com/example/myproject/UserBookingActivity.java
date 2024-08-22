4d69a72f80d28c10fb6d8bbb1fd2e615dc48b6b3
asrjashan <jashanchhapa85@gmail.com>
2024-08-22 06:53:44 -0400

    create UserBookingActivity

---
+++ app/src/main/java/com/example/myproject/UserBookingActivity.java
@@ -0,0 +1,24 @@
+package com.example.myproject;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class UserBookingActivity extends AppCompatActivity {
+    private android.widget.ListView listViewBookings;
+    private BookingAdapter bookingAdapter;
+    private java.util.List<HotelBooking> bookingList;
+
+    private com.google.firebase.database.DatabaseReference bookingsDB;
+    private com.google.firebase.auth.FirebaseUser currentUser;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_booking);
+
+    }
+}
